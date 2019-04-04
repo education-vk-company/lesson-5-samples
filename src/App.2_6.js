3dee8240b1664bfe0b3387da6734d2b8eff2314e
Martin Komitsky <m.komitsky@corp.mail.ru>
2019-04-04 09:28:35 +0300

    css loader + portal example

--- src/App.2_6.js
+++ src/App.2_6.js
@@ -4,13 +4,17 @@ import {
 	AppContext,
 	AppContext2,
 	AppContext3,
+	AppPortal,
 } from './2_6-react/';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
+				<AppContext />
+				<AppContext2 />
 				<AppContext3 />
+				<AppPortal />
 			</div>
 		);
 	}
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
 			test: /\.(js|jsx)$/,
 			exclude: /node_modules/,
 			use: ['babel-loader']
+		}, {
+			test: /\.css$/,
+			use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
 		}]
 	},
 	resolve: {
